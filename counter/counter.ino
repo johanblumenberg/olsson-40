b5d62d019262f8f971623f497c59047405ea0412
Olof Larsson <olof.larsson@symphony.com>
2019-02-23 01:21:01 +0100

    Countdown ddd hh:mm:ss.cc

--- counter/counter.ino
+++ counter/counter.ino
@@ -26,6 +26,7 @@ char ssid[] = "getskump4";  //  your network SSID (name)
 char pass[] = "getskump1234";       // your network password
 
 String line = "Olle är bäst på allt och snygg som fan" ;
+long long DATE = 1582588800000LL;
 
 void setup() {
   Serial.begin(115200);
@@ -62,11 +63,27 @@ void setup() {
 void loop() { 
   struct timeval tv;
   gettimeofday(&tv, nullptr);
-  Serial.print("gettimeofday() ");
+  long long now = 1000LL * tv.tv_sec + tv.tv_usec / 1000;
+  long long delta = DATE - now;
+
+  delta /= 10;
+  int cs = delta % 100;
+  delta /= 100;
+  int s = delta % 60;
+  delta /= 60;
+  int m = delta % 60;
+  delta /= 60;
+  int h = delta % 24;
+  delta /= 24;
+  int d = delta;
+  
   char tmp[16];
-  sprintf(tmp, "%d %02d", tv.tv_sec, tv.tv_usec / 10000);
+  sprintf(tmp, "%4d %02d:%02d:%02d.%02d", d, h, m, s, cs);
+
   Serial.println(tmp);
-  lcd.setCursor(0, 1);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print(tmp);
 
   delay(70); 

29966569340c55321ceee22552ae4ab366347e97
johanblumenberg <johan.blumenberg@gmail.com>
2019-02-27 23:31:49 +0100

    Panic mode

--- counter/counter.ino
+++ counter/counter.ino
@@ -28,6 +28,25 @@ char pass[] = "8005300167";
 const char line[] = "Game Over when counter reaches zero";
 long long DATE = 1582588800000LL;
 
+enum State {
+  COUNTDOWN,
+  PANIC,
+  BLANK,
+  ONEUP
+};
+
+State state(long long now) {
+  if (now < DATE + 10*1000) {
+    return ONEUP;
+  } else if (now < DATE + 25*1000) {
+    return BLANK;
+  } else if (now < DATE + 60*60*1000) {
+    return PANIC;
+  } else {
+    return COUNTDOWN;
+  }
+}
+
 long long time() {
   struct timeval tv;
   gettimeofday(&tv, nullptr);
@@ -95,6 +114,67 @@ void updateTime(long long now) {
   lcd.print(tmp);
 }
 
+void updatePanic(long long now) {
+  long long delta = DATE - now;
+
+  delta /= 10;
+  int cs = delta % 100;
+  delta /= 100;
+  int s = delta % 60;
+  delta /= 60;
+  int m = delta % 60;
+
+  char c1 = (now + 800) % 1700 < 300 ? '!' : ' ';
+  char c2 = (now + 400) % 2300 < 300 ? '!' : ' ';
+  char c3 = (now + 900) % 1100 < 300 ? '!' : ' ';
+  char c4 = (now + 200) % 1900 < 300 ? '!' : ' ';
+  char c5 = (now + 600) % 1800 < 300 ? '!' : ' ';
+  char c6 = (now + 100) % 2500 < 300 ? '!' : ' ';
+
+  const char* panic = (now % 1500 > 750) ? "PANIC!" : "      ";
+
+  char tmp[17];
+  sprintf(tmp, "%c%c%c %02d:%02d.%02d %c%c%c", c1, c2, c3, m, s, cs, c4, c5, c6);
+
+  Serial.println(tmp);
+  
+  lcd.setCursor(0, 0);
+  lcd.print(tmp);
+
+  sprintf(tmp, "%c%c %c %s %c %c%c", c5, c4, c6, panic, c1, c3, c2);
+
+  lcd.setCursor(0, 1);
+  lcd.print(tmp);
+}
+
+void updateBlank(long long now) {
+  Serial.println("blank");
+
+  char dot = (now % 3000 > 1500) ? '.' : ' ';
+
+  char tmp[17];
+  sprintf(tmp, " %c              ", dot);
+  
+  lcd.setCursor(0, 0);
+  lcd.print(tmp);
+  lcd.setCursor(0, 1);
+  lcd.print("                ");  
+}
+
+void updateOneUp(long long now) {
+  Serial.println("Extra Life!");
+
+  char excm = (now % 1500 > 750) ? '!' : ' ';
+
+  char tmp[17];
+  sprintf(tmp, " %c    Extra   %c ", excm, excm);
+  
+  lcd.setCursor(0, 0);
+  lcd.print(tmp);
+  lcd.setCursor(0, 1);
+  lcd.print("      Life!     ");  
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -134,8 +214,23 @@ void setup() {
 void loop() {
   long long now = time();
 
-  updateTime(now);
-  updateText(now);
+  switch (state(now)) {
+    case COUNTDOWN:
+      updateTime(now);
+      updateText(now);
+      break;
+
+    case PANIC:
+      updatePanic(now);
+      break;
+
+    case BLANK:
+      updateBlank(now);
+      break;
+      
+    case ONEUP:
+      updateOneUp(now);
+  }
 
   delay(70); 
 }
